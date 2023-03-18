7765010e19c7a539e45ae6094cc33009fc3855f8
Vitaliy Antipov <demjke64@gmail.com>
2023-03-18 04:04:06 +0400

    Sh_HW_1_4

---
+++ lesson_1/HW/HW_1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N..
+
+Console.WriteLine("Please input number");
+int a = int.Parse(Console.ReadLine()!);
+int b = 2;
+Console.WriteLine("Numbers: ");
+while (a >= b) {
+    Console.WriteLine(b);
+    b += 2;
+}
