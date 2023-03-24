2ebb17c299b6d71c7b5be7595b0fb3cb3fb3070e
Vitaliy Antipov <demjke64@gmail.com>
2023-03-25 02:44:17 +0400

    Task 3_1

---
+++ lesson_3/3_1/Program.cs
@@ -0,0 +1,14 @@
+﻿void Coordinate(int a) {
+    if (a == 1) {
+        Console.WriteLine("x > 0, y > 0");
+    } else if (a == 2) {
+        Console.WriteLine("x > 0, y < 0");
+    } else if (a == 3) {
+        Console.WriteLine("x < 0, y < 0");
+    } else {
+        Console.WriteLine("x < 0, y > 0");
+    }
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Coordinate(num);
