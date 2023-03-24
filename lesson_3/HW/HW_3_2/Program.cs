f7915dc32c88cea33a1b096c8527649ce2f2057b
Vitaliy Antipov <demjke64@gmail.com>
2023-03-25 03:30:57 +0400

    Sh_HW_3_2

---
+++ lesson_3/HW/HW_3_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double Trible(int x1, int y1, int z1, int x2, int y2, int z2) {
+    return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+}
+
+int x1 = int.Parse(Console.ReadLine()!);
+int y1 = int.Parse(Console.ReadLine()!);
+int z1 = int.Parse(Console.ReadLine()!);
+int x2 = int.Parse(Console.ReadLine()!);
+int y2 = int.Parse(Console.ReadLine()!);
+int z2 = int.Parse(Console.ReadLine()!);
+
+double res = Trible(x1, y1, z1, x2, y2, z2);
+Console.WriteLine(res);
