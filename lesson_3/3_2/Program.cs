0858a4b5eca31caf3ab5af419293bb4e50fecdc1
Vitaliy Antipov <demjke64@gmail.com>
2023-04-06 17:40:20 +0400

    ls3

--- lesson_3/3_2/Program.cs
+++ lesson_3/3_2/Program.cs
@@ -1,4 +1,4 @@
-﻿double Distance(int x1, int y1, int x2, int y2) {
+﻿double distance(int x1, int y1, int x2, int y2) {
     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 }
 
@@ -6,5 +6,5 @@ int x1 = int.Parse(Console.ReadLine()!);
 int y1 = int.Parse(Console.ReadLine()!);
 int x2 = int.Parse(Console.ReadLine()!);
 int y2 = int.Parse(Console.ReadLine()!);
-double result = Distance(x1, y1, x2, y2);
+double result = distance(x1, y1, x2, y2);
 Console.WriteLine(result);
