0d9858f8522c54bc0563507293062bb9994efe49
Vitaliy Antipov <demjke64@gmail.com>
2023-04-06 19:32:56 +0400

    Task 4_2

---
+++ lesson_4/4_2/Program.cs
@@ -0,0 +1,13 @@
+﻿int MulNum(int num) {
+    int all_mul = 1;
+
+    for(int i = 1; i <= num; i++) {
+        all_mul *= i;
+    }
+    return all_mul;
+}
+
+Console.WriteLine("Введите число");
+int val = int.Parse(Console.ReadLine()!);
+int result = MulNum(val);
+Console.WriteLine(result);
