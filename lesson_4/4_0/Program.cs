2d0fabfa508c98c3c87812ca1dab19a057604743
Vitaliy Antipov <demjke64@gmail.com>
2023-04-06 17:55:55 +0400

    Task 4_0

---
+++ lesson_4/4_0/Program.cs
@@ -0,0 +1,13 @@
+﻿int SumNum(int num) {
+    int all_sum = 0;
+
+    for(int i = 0; i <= num; i++) {
+        all_sum += i;
+    }
+    return all_sum;
+}
+
+Console.WriteLine("Введите число");
+int val = int.Parse(Console.ReadLine()!);
+int result = SumNum(val);
+Console.WriteLine(result);
