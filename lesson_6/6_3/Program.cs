f16b2d3e7121c1f3727be9e77b57774bfacdce0e
Vitaliy Antipov <demjke64@gmail.com>
2023-04-19 22:51:30 +0400

    Task 6_3

---
+++ lesson_6/6_3/Program.cs
@@ -0,0 +1,11 @@
+﻿void Fibb(int num) {
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < num; i++) {
+        Console.Write($"{a} ");
+        (a, b) = (b, a + b);         
+    }
+}
+
+int x = int.Parse(Console.ReadLine()!);
+Fibb(x);
