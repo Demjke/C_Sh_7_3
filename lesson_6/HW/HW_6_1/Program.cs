8a12e2b151a1da97633f375c851c2eaa77d2f89f
Vitaliy Antipov <demjke64@gmail.com>
2023-04-20 00:01:47 +0400

    Sh_HW_6_1

---
+++ lesson_6/HW/HW_6_1/Program.cs
@@ -0,0 +1,12 @@
+﻿string? x = Console.ReadLine();
+int count = 1;
+Console.WriteLine("Положительных чисел: " + count);
+while (x != "") {
+    int a = int.Parse(Console.ReadLine()!);
+    if (a > 0) {
+        count = count + 1;
+        Console.WriteLine("Положительных чисел: " + count);
+    } else {
+        Console.WriteLine("Не положительное");
+    }
+}
