e61465ddaf65fc894fbe778bfd8e3016293d2007
Vitaliy Antipov <demjke64@gmail.com>
2023-04-22 21:10:23 +0400

    Task 9_0

---
+++ lesson_9/9_0/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number N: ");
+int num = int.Parse(Console.ReadLine()!);
+
+void Natural (int N) {
+    if (N == 0) {
+        return;
+    } 
+    Natural(N - 1);
+    Console.Write($"{N} ");
+}
+Natural(num);
