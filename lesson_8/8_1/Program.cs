b05272e416a210df25be020164119fea04e85ecd
Vitaliy Antipov <demjke64@gmail.com>
2023-04-22 17:06:30 +0400

    Task 8_1

---
+++ lesson_8/8_1/Program.cs
@@ -0,0 +1,47 @@
+﻿void Print(int[,] arr) {
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++) {
+        for (int j = 0; j < column_size; j++) {
+            Console.Write($"{arr[i, j], 6} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MassNums(int row, int column, int from, int to) {
+    int[,] arr = new int[row, column];
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            arr[i, j] = new Random().Next(from, to + 1);
+        }
+    }
+    return arr;
+}
+
+void ChangeMtr(int[,] arr) { 
+    if (arr.GetLength(0) == arr.GetLength(1)) {
+        for (int i = 0; i < arr.GetLength(0); i++) {
+            for (int j = 0; j < i; j++) {
+                (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
+            }
+        }    
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the min number of massive: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
+ChangeMtr(mass);
+Console.WriteLine();
+Print(mass);
