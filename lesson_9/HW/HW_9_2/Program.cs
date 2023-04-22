f6240515ce058b4eb9f37633154824e540442f49
Vitaliy Antipov <demjke64@gmail.com>
2023-04-22 22:53:29 +0400

    Sh_HW_9_2

---
+++ lesson_9/HW/HW_9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Enter number M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int Natural (int M, int N) {
+    int sum = 0;
+    if (M > N) {
+        return sum;
+    } 
+    if (N % 2 != 0) {
+        return Natural(M, N - 1);
+    } 
+    if (N % 2 == 0) {
+        return sum += Natural(M, N - 2);
+    } else {
+        return sum;
+    }
+}
+Console.Write(Natural(m, n));
