6d763a7d2553d734da3d9275502feccd6d7e2add
Vitaliy Antipov <demjke64@gmail.com>
2023-04-22 21:54:12 +0400

    Task 9_1

---
+++ lesson_9/9_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter number M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void Natural (int M, int N) {
+    if (M > N) {
+        return;
+    } 
+    Natural(M, N - 1);
+    Console.Write($"{N} ");
+}
+Natural(m, n);
